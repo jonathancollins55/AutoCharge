7e60798beaea22ccf8ace5365af16f92f284d1cf
Jonathan Collins <jonathan@onetouchbilling.com>
2021-05-11 21:08:38 -0400

    Basic Finished

--- charger.py
+++ charger.py
@@ -1,7 +1,16 @@
 import kasa as ks
 import asyncio
+import psutil
+from dotenv import dotenv_values
 
+environment_vars = dotenv_values('keys.env')
+
+#Toggles the smartPlug on or off
 async def switch_smartPlug(ip_addr):
+    if ip_addr == None: return
+    if not(isinstance(ip_addr,str)): return
+
+    #Initialize the plug and await the update
     plug = ks.SmartPlug(ip_addr)
     await plug.update()
     if plug.is_on:
@@ -9,4 +18,21 @@ async def switch_smartPlug(ip_addr):
         return
     await plug.turn_on()
 
-asyncio.run(switch_smartPlug('192.168.1.165'))
+async def switchPlug_whenBatteryLow(ip_addr):
+    if ip_addr == None: return
+    if not(isinstance(ip_addr,str)): return
+
+    #Get battery percentage
+    battery = psutil.sensors_battery()
+    threshold_percentage = bool(battery.percent > 30)
+
+    #Turn plug on of battery is less than 30%
+    plug = ks.SmartPlug(ip_addr)
+    await plug.update()
+    if threshold_percentage:
+        await plug.turn_off()
+        return
+    await plug.turn_on()
+
+asyncio.run(switchPlug_whenBatteryLow(environment_vars.get('Plug_ip_addr')))
+# asyncio.run(switch_smartPlug(environment_vars.get('Plug_ip_addr')))
